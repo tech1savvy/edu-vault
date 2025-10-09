0ab7753d15b61b5896709168095041bb049725a7
tech1savvy <amankumar010604@gmail.com>
2025-10-09 10:53:47 +0530

    fix: resume context import paths

--- client/src/components/Resume/Education.jsx
+++ client/src/components/Resume/Education.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { ResumeContext } from "../../context/ResumeContext";
+import { ResumeContext } from "../../context/resumeContext";
 
 function Education() {
   const { educationData } = useContext(ResumeContext);
