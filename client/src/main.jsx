2f930cd887639682a88fec97e1749abe4bb8a808
tech1savvy <amankumar010604@gmail.com>
2025-11-15 08:08:10 +0530

    breaking: switched form react-router to react-router-dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import { ResumeProvider } from "./context/resumeContext";
 import "./index.css";
 import App from "./App.jsx";
@@ -14,4 +14,3 @@ createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </StrictMode>,
 );
-
