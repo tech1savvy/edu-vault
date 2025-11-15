af81a13544affceb159dbea1b58c383cd4498b5a
tech1savvy <amankumar010604@gmail.com>
2025-11-15 08:06:58 +0530

    add: login and singup routes links

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -123,6 +123,14 @@ function Navbar() {
                   </Link>
                 </li>
               </ul>
+            </li>
+          </ul>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">Login</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/signup">Sign Up</Link>
             </li>
           </ul>
         </div>
