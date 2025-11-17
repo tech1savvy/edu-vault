71e649254043b1be5157b044d4e7dde67274a0c6
tech1savvy <amankumar010604@gmail.com>
2025-11-17 13:09:20 +0530

    feat(ml): Scaffold ML service structure

---
+++ server/features/ml/index.js
@@ -0,0 +1,2 @@
+// features/ml/index.js
+// This file will export the ML services for use in other parts of the application.
---
+++ server/features/ml/pinecone.service.js
@@ -0,0 +1,2 @@
+// features/ml/pinecone.service.js
+// This service will manage the Pinecone client and all interactions with the Pinecone API.
