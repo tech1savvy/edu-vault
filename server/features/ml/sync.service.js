c3bddd7869e565bc6f08d928e18c009234346997
Avjot Singh <asinghchatrath@gmail.com>
2026-02-20 21:04:51 +0530

    restore(pinecone): reintroduce pinecone service alongside qdrant

--- server/features/ml/sync.service.js
+++ server/features/ml/sync.service.js
@@ -1,8 +1,7 @@
 const { getAggregatedResumeText } = require('../resume/resume.aggregator.js');
 const EmbeddingService = require('./embedding.service');
+const PineconeService = require('./pinecone.service');
 const logger = require('../../config/logger');
-const userRepository = require('../user/user.repository');
-const jobDescriptionRepository = require('../job-description/jobDescription.repository');
 
 const syncResume = async (userId) => {
   try {
@@ -23,6 +22,7 @@ const syncResume = async (userId) => {
       },
     };
 
+    await PineconeService.upsert([vector]);
     logger.info(`Successfully synced resume for user: ${userId}`);
   } catch (error) {
     logger.error(`Error syncing resume for user ${userId}:`, { message: error.message, stack: error.stack });
@@ -43,34 +43,16 @@ const syncJobDescription = async (jobDescription) => {
       },
     };
 
-
+    await PineconeService.upsert([vector]);
     logger.info(`Successfully synced job description for job: ${jobDescription.id}`);
   } catch (error) {
     logger.error(`Error syncing job description for job ${jobDescription.id}:`, { message: error.message, stack: error.stack });
   }
 };
 
-const syncAll = async () => {
-  logger.info('Starting bulk synchronization for all users and job descriptions...');
-
-  const users = await userRepository.findAll();
-  for (const user of users) {
-    // Not awaiting each one to allow them to run in parallel without blocking
-    syncResume(user.id);
-  }
-
-  const jobDescriptions = await jobDescriptionRepository.findAll();
-  for (const job of jobDescriptions) {
-    syncJobDescription(job);
-  }
-
-  logger.info('Bulk synchronization process initiated for all items.');
-};
-
 const SyncService = {
   syncResume,
   syncJobDescription,
-  syncAll,
 };
 
 module.exports = { SyncService };
