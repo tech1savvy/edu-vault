aff959897f96b560508a1d3dffaeccd9b0e402ed
tech1savvy <amankumar010604@gmail.com>
2026-02-20 07:35:26 +0530

    refactor(client): update react-router-dom imports and remove unused jwt-decode dependency
    The `react-router` import has been updated to `react-router-dom` to reflect the correct package name.
    The `jwt-decode` dependency was removed as it is no longer used in the application.
    This change aims to clean up dependencies and ensure correct imports for routing.

--- client/src/components/Resume/Forms/EducationForm.jsx
+++ client/src/components/Resume/Forms/EducationForm.jsx
@@ -1,5 +1,5 @@
 import { useState, useContext } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import { ResumeContext } from "../../../context/resumeContext";
 
 function EducationForm() {
